cd5f0ff31cf1e319d36fae632267437bf633c585
Deniz Aksoylu <aksoyludeniz@outlook.com>
2018-03-11 20:59:20 -0400

    changes made

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,26 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "dudewasup23",
-  database: "burger_db"
-});
+if (process.env.NODE_ENV === "production") {
+
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "o336wzdttql8mahy",
+    password: "h10ws08po54s2gz3",
+  });
+} else {
+  var connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "dudewasup23",
+    database: "burger_db"
+  });
+
+}
+
+
+
 
 // Make connection.
 connection.connect(function(err) {
